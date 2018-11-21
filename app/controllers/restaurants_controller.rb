69322fa79f3e8bddf0736473e63ed5d102929655
Vincent Degove <vincent.degove@muage.net>
2018-11-21 14:36:20 +0100

    Add index, show, new and create methods and views to restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,28 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    set_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
